324317d217d5454bcbfb2f00c5329acffd9f61a6
StopStone <wltjr6432@naver.com>
2025-12-18 23:06:06 +0900

    feat: 수취 국가 선택 휠 피커 컴포넌트 추가

---
+++ app/src/main/java/com/codingtestmobile/currencyexchange/feature/components/CountryPicker.kt
@@ -0,0 +1,24 @@
+package com.codingtestmobile.currencyexchange.feature.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.codingtestmobile.currencyexchange.domain.model.Country
+import com.codingtestmobile.currencyexchange.ui.components.TextWheelPicker
+
+@Composable
+fun CountryPicker(
+    selectedCountry: Country,
+    onCountrySelected: (Country) -> Unit,
+) {
+    TextWheelPicker(
+        items = Country.entries,
+        initialIndex = Country.entries.indexOf(selectedCountry),
+        onItemSelected = { _, country -> onCountrySelected(country) },
+        modifier =
+            Modifier
+                .fillMaxWidth(),
+        visibleItemCount = Country.entries.size,
+        itemToString = { it.displayName },
+    )
+}
