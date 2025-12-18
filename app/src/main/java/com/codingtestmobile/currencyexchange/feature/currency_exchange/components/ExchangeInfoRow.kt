8b0e48b8bb8fa43f08e1a62eae90645a904d97f8
StopStone <wltjr6432@naver.com>
2025-12-18 23:24:50 +0900

    refactor: ExchangeInfoRow 정렬 수정

--- app/src/main/java/com/codingtestmobile/currencyexchange/feature/currency_exchange/components/ExchangeInfoRow.kt
+++ app/src/main/java/com/codingtestmobile/currencyexchange/feature/currency_exchange/components/ExchangeInfoRow.kt
@@ -1,11 +1,14 @@
 package com.codingtestmobile.currencyexchange.feature.currency_exchange.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -19,8 +22,13 @@ fun ExchangeInfoRow(
             modifier
                 .fillMaxWidth()
                 .padding(vertical = 4.dp),
+        horizontalArrangement = Arrangement.Start,
     ) {
-        Text(text = label)
+        Text(
+            text = label,
+            modifier = Modifier.width(80.dp),
+            textAlign = TextAlign.End,
+        )
         Text(text = value)
     }
 }
